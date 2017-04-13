f24f853886c1269005c6d6709741c63bb833a1f2
Max Ferguson <maxkferg@gmail.com>
2017-04-12 23:23:16 -0700

    Added avatar url field

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
     name: 'nitrolabs:dropbox-oauth',
     summary: "Login service for dropbox accounts",
-    version: '1.3.0',
-    git: 'https://github.com/NitroLabs/meteor-dropbox-oauth'
+    version: '1.4.0',
+    git: 'https://github.com/Nitrolabs/meteor-dropbox-oauth'
 });
 
 Package.onUse(function(api) {
